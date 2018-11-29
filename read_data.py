dcec0cecc79e0769bbf184a63417e5bf220c2c26
Jagan <jaganshanmugam@outlook.com>
2018-11-30 00:21:45 +0100

    Read data from WESAD

--- read_data.py
+++ read_data.py
@@ -4,7 +4,7 @@ data_set_path = "/media/jac/New Volume/Datasets/WESAD"
 subject = 'S3'
 
 
-def load_data(self, path, subject):
+def load_data(path, subject):
     """Given path and subject, load the data of the subject"""
     os.chdir(path)
     os.chdir(subject)
@@ -12,30 +12,46 @@ def load_data(self, path, subject):
         data = pickle.load(file, encoding='latin1')
     return data
 
-class read_data:
+class read_data_of_one_subject:
     """Read data from WESAD dataset"""
-    def __init__(self, data):
+    def __init__(self, path, subject):
         self.keys = ['label', 'subject', 'signal']
         self.signal_keys = ['wrist', 'chest']
         self.chest_sensor_keys = ['ACC', 'ECG', 'EDA', 'EMG', 'RESP', 'TEMP']
         self.wrist_sensor_keys = ['ACC', 'BVP', 'EDA', 'TEMP']
+        os.chdir(path)
+        os.chdir(subject)
+        with open(subject + '.pkl', 'rb') as file:
+            data = pickle.load(file, encoding='latin1')
         self.data = data
 
     def get_label(self):
         return self.data[self.keys[0]]
 
     def get_wrist_data(self):
-        label = self.data[self.keys[0]]
+        #label = self.data[self.keys[0]]
         assert subject == self.data[self.keys[1]]
         signal = self.data[self.keys[2]]
         wrist_data = signal[self.signal_keys[0]]
+        #wrist_ACC = wrist_data[self.wrist_sensor_keys[0]]
+        #wrist_ECG = wrist_data[self.wrist_sensor_keys[1]]
+        return wrist_data
+
+    def get_chest_data(self):
+        signal = self.data[self.keys[2]]
         chest_data = signal[self.signal_keys[1]]
-        wrist_ACC = wrist_data[self.wrist_sensor_keys[0]]
-        wrist_ECG = wrist_data[self.wrist_sensor_keys[1]]
+        return chest_data
 
 if __name__ == '__main__':
     #print(label.size)  # 4545100
     #print(wrist_ACC.size)  # 623328
     #print(wrist_ECG.size)  # 415552
+    #data = load_data(data_set_path,subject)
+    obj_data = {}
+    obj_data[subject] = read_data_of_one_subject(data_set_path, subject)
+    #print(obj_data[subject].data)
+    #print(obj_data[subject].get_label())
+    wrist_data_dict = obj_data[subject].get_wrist_data()
+    print(wrist_data_dict['ACC'])
     print("Test")
     #Flow: Read data for all subjects -> Extract features (Preprocessing) -> Train the model
